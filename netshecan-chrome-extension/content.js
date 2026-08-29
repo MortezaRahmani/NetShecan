47750e5d37537414e03ddd226ca86fd1046e8705
Morteza Rahmani <rahmani.morteza@gmail.com>
2026-08-29 18:17:44 +0330

    Add TCI browser extension support

--- netshecan-chrome-extension/content.js
+++ netshecan-chrome-extension/content.js
@@ -71,11 +71,29 @@
     };
   }
 
+  function asciiDigits(value) {
+    return String(value).replace(/[۰-۹٠-٩]/g, function (digit) {
+      const code = digit.charCodeAt(0);
+      return String(code >= 0x06F0 ? code - 0x06F0 : code - 0x0660);
+    });
+  }
+
+  function buildTci() {
+    const match = document.body.innerText.match(/\(([\d۰-۹٠-٩]+)\)/);
+    if (!match) return null;
+    return {
+      provider: "tci",
+      username: asciiDigits(match[1]),
+      panel_url: "https://internet.tci.ir/panel/",
+    };
+  }
+
   function build() {
     const host = location.hostname;
     if (host === "my.irancell.ir") return buildIrancell();
     if (host === "beta.my.shatel.ir") return buildShatel();
     if (host === "my.mci.ir") return buildMci();
+    if (host === "internet.tci.ir" || host === "adsl.tci.ir") return buildTci();
     return null;
   }
 
